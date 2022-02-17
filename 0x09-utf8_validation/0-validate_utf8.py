e67841a933ac14d9809450e1f72ce07fcc1e043a
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-02-17 15:26:06 -0500

    debuging

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,20 +1,41 @@
+
 #!/usr/bin/python3
+"""Determines a valid UTF-8 encoding"""
+
+
 def validUTF8(data):
-    counter = 0
+    """
+    bit1 checks if significant byte is 1
+    bit2 checks if second significant byte is 0
+    nbytes keeps track of how many 1s before 0 occurs
+    data represented by a list of integers to check
+    """
+
+    bit1 = 1 << 7
+    bit2 = 1 << 6
+    nbytes = 0
+
+    if not data or len(data) == 0:
+        return True
+
     for num in data:
-        mask = 0b10000000
-        if not counter:
-            while (mask & num):
-                counter += 1
-                mask >>= 1
-            if counter > 4:
+        bit = 1 << 7
+        if nbytes == 0:
+            while (bit & num):
+                nbytes += 1
+                bit = bit >> 1
+
+            if nbytes == 0:
+                continue
+            if nbytes == 1 or nbytes > 4:
                 return False
-            if counter:
-                counter -= 1
-                if counter == 0:
-                    return False
-        elif counter > 0:
-            if num >> 6 != 2:
+        else:
+
+            if not (num & bit1 and not (num & bit2)):
                 return False
-            counter -= 1
-    return not counter
+        nbytes -= 1
+
+    if nbytes:
+        return False
+    else:
+        return True
